75e95cbc1ec31f77d4ca30038fb7cea5a576d269
Zhang Cheng <zhangcheng@Zhangs-MacBook-Pro.local>
2017-02-02 14:06:27 -0800

    modified

---
+++ Leetcode_LinkedList/Sort_List.py
@@ -0,0 +1,58 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+#method 1: sort in string and transform to linked list
+class Solution(object):
+    def sortList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        cur=head
+        str_node=[]
+        while cur<>None:
+            str_node.append(cur.val)
+            cur=cur.next
+        str_node.sort()
+        cur=head
+        for i in str_node:
+            cur.val=i
+            cur=cur.next
+        return head
+#method 2: iterative
+#  def merge(self, head1, head2):
+#         if head1 == None: return head2
+#         if head2 == None: return head1
+#         dummy = ListNode(0)
+#         p = dummy
+#         while head1 and head2:
+#             if head1.val <= head2.val:
+#                 p.next = head1
+#                 head1 = head1.next
+#                 p = p.next
+#             else:
+#                 p.next = head2
+#                 head2 = head2.next
+#                 p = p.next
+#         if head1 == None:
+#             p.next = head2
+#         if head2 == None:
+#             p.next = head1
+#         return dummy.next
+
+#  def sortList(self, head):
+#         if head == None or head.next == None:
+#             return head
+#         slow = head; fast = head
+#         while fast.next and fast.next.next:
+#             slow = slow.next
+#             fast = fast.next.next
+#         head1 = head
+#         head2 = slow.next
+#         slow.next = None
+#         head1 = self.sortList(head1)
+#         head2 = self.sortList(head2)
+#         head = self.merge(head1, head2)
+#         return head
